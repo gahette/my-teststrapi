3875b6bf13f46a68787020fdf46910eb02c520d9
gahette <gahette@icloud.com>
2022-03-30 18:54:21 +0200

    init 3

--- config/server.js
+++ config/server.js
@@ -1,3 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('https://my-teststrapi.herokuapp.com/'),
+  proxy: true,
+  app: {
+    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+  },
+});
 });
